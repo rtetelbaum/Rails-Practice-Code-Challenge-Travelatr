88743dc041f9a301e23f0793f8eef85d6d66fda4
miShelbyT <mi.shelbyrose@gmail.com>
2020-10-13 16:30:07 -0400

    continuing with deliverables

--- app/controllers/bloggers_controller.rb
+++ app/controllers/bloggers_controller.rb
@@ -2,4 +2,20 @@ class BloggersController < ApplicationController
 	def show
 		@blogger = Blogger.find(params[:id])
 	end
+
+	def create
+		@blogger = Blogger.new(blogger_params)
+		if @blogger.save
+			redirect_to blogger_path(@blogger)
+		else
+			flash[:errors] = @blogger.errors.full_messages
+			redirect_to new_blogger_path
+		end
+	end
+
+	private
+
+	def blogger_params
+		params.require(:blogger).permit(:name, :bio, :age)
+	end
 end
